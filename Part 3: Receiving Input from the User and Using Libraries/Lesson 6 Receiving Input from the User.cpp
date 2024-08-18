a06e65e9b05a78dfc6b51308a07a4ee466889c35
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-18 22:07:45 +0300

    Add files via upload

---
+++ Part 3: Receiving Input from the User and Using Libraries/Lesson 6 Receiving Input from the User.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	string name;
+	int width, height, area;
+
+	cout << "Please enter your name: ";
+	cin >> name;
+
+	cout << "Hello " << name << endl << "Welcome to the geometric calculation program" << endl;
+	
+	cout << "Please enter the width of the rectangle: ";
+	cin >> width;
+
+	cout << "Please enter the height of the rectangle: ";
+	cin >> height;
+
+	//Calculating...
+	area = width * height;
+	cout << "Mrs. " << name << " your result is: " << area << endl;
+
+	return 0;
+}
