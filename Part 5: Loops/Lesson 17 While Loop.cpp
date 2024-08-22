8e147513c596d8347b1f9b7eac8e82e52d96a89b
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 23:53:46 +0300

    Add files via upload

---
+++ Part 5: Loops/Lesson 17 While Loop.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	/*
+				While Loop
+	It continues until a predetermined condition is broken.
+	After each check of the condition, the operations inside the loop are executed once.
+	The loop is exited on the first check after the condition is broken.
+
+	while (condition)
+		// operation 1
+		// operation 2
+		// ...
+	*/
+
+	int index = 0;
+	while (index <= 3) {
+		cout << "The value of index is smaller than 3" << endl;
+		index++;
+	}
+	cout << "Index: " << index << endl;
+	cout << "The end of the program." << endl;
+
+	return 0;
+}
