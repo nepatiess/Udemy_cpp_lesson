a2bc47a6ee63ef45abe0acb649ab1aa68514442b
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 19:39:08 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 12 Example.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+// kullanıcıdan 3 sayı isteyin ve bu sayılardan en küçüğünü bulan program yazın
+	
+	int num1, num2, num3;
+	cout << "----------------------------------" << endl;
+	cout << "Please enter the first number: ";
+	cin >> num1;
+	cout << "Please enter the second number: ";
+	cin >> num2;
+	cout << "Please enter the third number: ";
+	cin >> num3;
+
+	if (num1 < num2 && num1 < num3) {
+		cout << "The smallest of the 3 numbers: " << num1 << endl;
+	}
+	else if (num2 < num1 && num2 < num3) {
+		cout << "The smallest of the 3 numbers: " << num2 << endl;
+	}
+	else if (num3 < num1 && num3 < num2) {
+		cout << "The smallest of the 3 numbers: " << num3 << endl;
+	}
+	else if (num1 == num2 || num2 == num3 || num1 == num3) {
+		cout << "There is no smallest number." << endl;
+	}
+	else {
+		cout << "Invalid section.";
+	}
+
+	return main();
+}
