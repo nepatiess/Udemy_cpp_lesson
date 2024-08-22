aa8d73c607f69d1b72d1ba61f814eed4a885c0cf
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-23 00:05:47 +0300

    Add files via upload

---
+++ Part 5: Loops/Lesson 18 While loop example.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	
+/* Get 2 numbers from the user.
+Name them as the start and end numbers.
+Display all the numbers between the start and end numbers 
+that are divisible by 7.
+*/
+	int start_num, end_num;
+	int index;
+	cout << "Please enter the start number: ";
+	cin >> start_num;
+	cout << "Please enter the end number: ";
+	cin >> end_num;
+
+	index = start_num;
+	while (index <= end_num) {
+		
+		if (index % 7 == 0) {
+			cout << index << " ";
+		}
+		index++;
+	}
+	return 0;
+}
