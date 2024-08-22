dc2166ebd0b2b961948b088679f049ea07a78af2
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 16:45:42 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 11 Comparison & Logical operations.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Please enter the value of a: ";
+	cin >> a;
+
+	cout << "Please enter the value of b: ";
+	cin >> b;
+
+	if (a == b) {
+		cout << "a == b" << endl;
+		cout << "a and b are equal." << endl;
+	}
+	else if (a != b) {
+		cout << "a != b" << endl;
+		cout << "a and b are not equal." << endl;
+	}
+	else if (a < b) {
+		cout << "a < b" << endl;
+		cout << "a is less than b." << endl;
+	}
+	else if (a > b) {
+		cout << "a > b" << endl;
+		cout << "a is greater then b." << endl;
+	}
+	else if (a <= b) {
+		cout << "a <= b" << endl;
+		cout << " a is less than equal to b." << endl;
+	}
+	else if (a >= b) {
+		cout << "a >= b" << endl;
+		cout << "a is greater than or equal to b." << endl;
+	}
+	else {
+		cout << "Invalid choice.";
+	}
+
+	int x = 15, y = 30;
+
+	cout << "x = 15" << endl;
+	cout << "y = 30" << endl;
+
+	if (x > 10 && y > 20) {
+		cout << "x is greater than 10 AND y is greater than 20." << endl;
+	}
+
+	if (x < 20 || y < 40) {
+		cout << "x is less than 20 OR y is less than 40." << endl;
+	}
+
+	if (!(x > 10)) {
+		cout << "x is not greater than 10." << endl;
+	}
+
+	return 0;
+}
