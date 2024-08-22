00d06a2c88ed73cbce5bf980d22f10c1a7ffa419
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 20:12:05 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 15 Interview quesiton 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	//What happens to the w output after running the following code blocks?
+
+	/*		A.
+	
+	int x = 5, y = 9, z = 7.4, w;
+	w = (z > x) - (y <= 11);
+	//    = 1   -    = 1     = the output:0
+	cout << w;
+
+	*/
+
+	/*		B.
+	
+	int x = 5, y = 9, z = 7.4, w;
+	w = z > x - y <= 11;
+	//First of all, x - y 
+	//w = z > (x - y) <=11
+	//Then, z > -4
+	//w = (z > (x - y)) <= 11 = the output: 1
+	cout << w;
+
+	*/
+
+	return 0;
+}
