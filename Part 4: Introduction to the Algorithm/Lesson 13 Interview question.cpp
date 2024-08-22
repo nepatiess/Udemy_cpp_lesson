aa5a961bd113b4bc76ba51bf9ce92f98189045e5
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 19:46:20 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 13 Interview question.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+	x=5 and y=10
+	what is the output?
+
+
+	int x = 5;
+	int y = 10;
+
+	if (x = 20) { // the "=" is wrong.
+		if (y == 20) {
+			cout << "** ";
+		}
+		else cout << "*****"; // there is no "{}" signs.
+	*/
+
+	//Here is the correct one
+	int x = 5;
+	int y = 10;
+
+	if (x == 20) {
+		if (y == 20) {
+			cout << "** ";
+		}
+		else cout << "*****";
+
+	}
+}
