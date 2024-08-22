3236eb0bdd4b7b13f5c639f549aaa40ce8b816c2
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 16:02:30 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 8 If-Else Condition.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+/*	if (condition) {
+
+		actions to be taken
+	}
+	else {
+
+		actions to be taken
+	}
+*/
+int main() {
+
+	int temp = 40;
+	
+	if (temp >= 35 ) {
+		
+		cout << "The environment is hot.";
+	}
+	else {
+		cout << "The environment is cold.";
+	}
+	return 0;
+}
