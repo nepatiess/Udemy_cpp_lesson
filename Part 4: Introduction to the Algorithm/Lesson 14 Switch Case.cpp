0b29192ed4ecbb43af212a5c000a5f6afc22386d
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 19:55:09 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 14 Switch Case.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	int day;
+	cout << "Please enter the days of the week as a number: ";
+	cin >> day;
+
+	switch (day) {
+
+	case 1:
+		cout << "Monday" << endl;
+		break;
+
+	case 2:
+		cout << "Tuesday" << endl;
+		break;
+
+	case 3:
+		cout << "Wednesday" << endl;
+		break;
+
+	case 4:
+		cout << "Thursday" << endl;
+		break;
+
+	case 5:
+		cout << "Friday" << endl;
+		break;
+
+	case 6:
+		cout << "Saturday" << endl;
+		break;
+
+	case 7:
+		cout << "Sunday" << endl;
+		break;
+
+	default:
+		cout << "You entered wrong number. Please enter a value between 1 and 7 " << endl;
+
+	}
+	return main();
+}
