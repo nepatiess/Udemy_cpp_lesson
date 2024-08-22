9fd55ed0a3baea1b3b4fbcec70c494821be97b80
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-22 16:08:24 +0300

    Add files via upload

---
+++ Part 4: Introduction to the Algorithm/Lesson 9 If else example code.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	
+	string username;
+	cout << "Please enter username: ";
+	cin >> username;
+	int password = 1234;
+	int userInput;
+	cout << "Please enter password: ";
+	cin >> userInput;
+
+	if (userInput == password) {
+
+		cout << "Welcome to the system, " << username;
+	}
+	else {
+		cout << "Password is wrong.";
+	}
+
+	return 0;
+}
