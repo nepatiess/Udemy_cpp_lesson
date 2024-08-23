d7699d4014ae9c8a1e30fc66d762fdc4d8442623
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-23 19:26:19 +0300

    Add files via upload

---
+++ Part 5: Loops/Lesson 19 Do-While Loop.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	/*
+	do {
+		// operation 1
+		// operation 2
+		// ...
+	} while (condition)
+
+	*/
+
+	int password = 1234;
+	int input;
+
+	do {
+		cout << "Please enter the password: ";
+		cin >> input;
+	} while (input != password);
+	//             true
+	// again do loop
+
+	cout << "Welcome. " << endl;
+	return 0;
+}
