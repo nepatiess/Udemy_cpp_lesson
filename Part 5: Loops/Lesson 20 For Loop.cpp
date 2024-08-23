fa9abb64d18c0d6de3c35354bef3b5cc8aed5b8c
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-23 19:34:09 +0300

    Add files via upload

---
+++ Part 5: Loops/Lesson 20 For Loop.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	/*
+	The for loop is basically a block of code for a certain number of 
+		and it is a loop that is used to run on top of each other.
+	*/
+
+	// Code a for loop that prints all the numbers from 1 to 10 on the screen.
+
+	for (int i = 1; i <= 10; i++) {
+		// transactions: 1, 2, 3, 4...
+		cout << i << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
