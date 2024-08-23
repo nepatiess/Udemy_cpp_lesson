8f2681e39f441f87ec46295ec50c0ce3dbfed1a9
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-23 19:45:23 +0300

    Add files via upload

---
+++ Part 5: Loops/Lesson 21 Exam Question.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	/*
+	Write a C++ program that calculates the factorial 
+		of the number that the user enters through the console
+	*/
+
+	int num, result = 1;
+	cout << "Please enter a number: ";
+	cin >> num;
+	
+	for (int i = 1; i <= num; i++) {
+		result = result * i;
+	}
+
+	cout << "The factorial of the " << num << " is " << result << endl;
+
+	return 0;
+}
