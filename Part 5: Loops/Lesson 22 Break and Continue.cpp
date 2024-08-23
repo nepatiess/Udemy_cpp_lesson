b1866ddf1a7c51540b53665df8080abf828984fc
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-23 19:52:13 +0300

    Add files via upload

---
+++ Part 5: Loops/Lesson 22 Break and Continue.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	//break: ends the cycle. (exit the loop)
+	//continue: the code sections under the continue keyword are skipped and the loop is continued
+
+	for (int i = 0; i <= 5; i++) {
+		if (i == 3)
+			break;
+		cout << i << " ";
+	}
+	cout << endl;
+	// 0 1 2
+
+	for (int k = 0; k <= 5; k++) {
+		if (k == 3)
+			continue;
+		cout << k << " ";
+	}
+	cout << endl;
+	// 0 1 2 4 5
+
+	return 0;
+}
